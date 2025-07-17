ddca2fef5d9c2fe5359bb6a90396006b8798dcbd
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-17 13:54:12 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="bg-[#1E1E1E]">
+    <div class="text-white flex flex-col gap-16 max-w-[1224px] w-full mx-auto">
+      <CharactersListing />
+      <EpisodesListing />
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import CharactersListing from "@/components/CharactersListing/index.vue";
+import EpisodesListing from "@/components/EpisodesListing/index.vue";
+</script>
